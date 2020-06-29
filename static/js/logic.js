6c493638630d9d352cc212a18c9334ecbff421a8
lisaashway <lisaashway@gmail.com>
2020-06-29 19:35:50 -0400

    gitignore

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,10 @@
-// Creating map object
+// map object with center at center of US
 var myMap = L.map("map", {
   center: [39.8283, -98.5795],
   zoom: 4
 });
 
-// Adding tile layer
+//  tile layer
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,25 +14,25 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   accessToken: API_KEY
 }).addTo(myMap);
 
-// Load in geojson data
+// geojson data
 var geoData = "static/data/Tribal_Lands.geojson";
 
 var geojson;
 
-// Grab data with d3
+// using d3
 d3.json(geoData, function(data) {
 
-  // Create a new choropleth layer
+  // adding choropleth layer
   geojson = L.choropleth(data, {
 
 
-    // Define what  property in the features to use
+    // feature property
     valueProperty: "TribalCededLandsTableCessNum",
 
-    // Set color scale
+    // color scale
     scale: ["#F333FF", "#33FFFC"],
 
-    // Number of breaks in step range
+    // number of breaks in color scale
     steps: 10,
 
     // q for quartile, e for equidistant, k for k-means
@@ -44,14 +44,14 @@ d3.json(geoData, function(data) {
       fillOpacity: 0.8
     },
 
-    // Binding a pop-up to each layer
+    // binding a pop-up to each layer
     onEachFeature: function(feature, layer) {
      
       layer.bindPopup("Name of Tribes: " + feature.properties.TribalCededLandsTableSchdTrb + "<br>Year:</br>" + feature.properties.TribalCededLandsTableCessDate1);
     }
   }).addTo(myMap);
 
-  // Set up the legend
+  // legend
   var legend = L.control({ position: "bottomright" });
   legend.onAdd = function() {
     var div = L.DomUtil.create("div", "info legend");
